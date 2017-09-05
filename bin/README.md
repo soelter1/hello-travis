73c27f14aa55f0a265b98fcdda664dcc4f77a662
Björn Sölter <Soelter@STUDENTS>
2017-09-05 15:12:41 +0200

    Aufg 1b

---
+++ bin/README.md
@@ -0,0 +1,3 @@
+# hello-travis
+
+Travis status: ![travis status](https://travis-ci.org/umr-programmierpraktikum-2017/hello-travis.svg?branch=master)
--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -11,7 +11,7 @@ public class Hello {
 	 * @return "Hello Travis!"
 	 */
 	static public String sayHelloTravis() {
-		return "Hello Travis ";
+		return "Hello World ";
 	}; 
 
 	/**
